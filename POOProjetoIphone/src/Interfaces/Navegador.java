99ee1caf01f678822980176a5206cb151c6adb7c
Alex <152513181+Pinheiro-Alex@users.noreply.github.com>
2024-10-14 21:06:14 -0300

    Add files via upload

---
+++ POOProjetoIphone/src/Interfaces/Navegador.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface Navegador {
+
+    public void exibirPagina(String url);
+
+    public void adicionarNovaAba();
+
+    public void atualizarPagina();
+}
---
+++ POOProjetoIphone/src/Interfaces/ReprodutorMusical.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public interface ReprodutorMusical{
+
+    public void tocar();
+
+    public void pausar();
+
+    public void selecionarMusica(String musica);
+
+}
